9a6636dfc7ae03cca3388a99370760f123925877
Addison <addison.frei@icloud.com>
2022-06-06 14:32:24 -0400

    Search by state event added

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ const state = document.querySelector('#submit-state')
 const city = document.querySelector('#submit-city')
 const brewName = document.querySelector('#submit-name')
 const inputType = document.querySelector('#input-type')
+const inputName = document.querySelector('#input-name')
+const inputCity = document.querySelector('#input-city')
+const inputState = document.querySelector('#input-state')
 
 
-// Add version of this request to an event listener for the 'submit' button
 fetch('https://api.openbrewerydb.org/breweries')
 .then(response => response.json())
 .then(item => {
@@ -34,7 +36,7 @@ fetch('https://api.openbrewerydb.org/breweries')
         p.innerText = `URL: ${item.website_url}`
     }
    
-    // Create card for each object and add the info listed above to elements.  Add h2 for name of brewery, ul and li for the address, phone, url.
+   
     // Add forward and back buttons at the bottom to advance through searches
 })
 
@@ -68,8 +70,31 @@ inputType.value = ''
 
 state.addEventListener('click', e => {
     e.preventDefault()
-    console.log('state was clicked')
+    console.log(inputState.value)
+    catalog.innerHTML = ''
+    fetch(`https://api.openbrewerydb.org/breweries?by_state=${inputState.value}&per_page=25`)
+    .then(response => response.json())
+    .then(item => {
+        for(item of item) {
+            const card = document.createElement('div')
+            const h3 = document.createElement('h3')
+            const ul = document.createElement('ul')
+            const li = document.createElement('li')
+            const p = document.createElement('p')
+            const catalog = document.querySelector('#brew-card')
+            card.classList.add('card')
+            catalog.appendChild(card)
+            card.appendChild(h3)
+            h3.innerText = item.name
+            card.appendChild(ul)
+            ul.appendChild(li)
+            li.innerText = `Address: ${item.street} \n City: ${item.city} \n State: ${item.state} \n Zip: ${item.postal_code} \n Phone: ${item.phone}`
+            card.appendChild(p)
+            p.innerText = `URL: ${item.website_url}`
+    }
 });
+inputState.value = ''
+})
 
 city.addEventListener('click', e => {
     e.preventDefault()
