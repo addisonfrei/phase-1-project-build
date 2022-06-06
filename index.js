9c2ed88ef1db7b5aa92ce7e52b48f78f9be9d830
Addison <addison.frei@icloud.com>
2022-06-06 14:23:00 -0400

    Search by type event listener added

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const ul = document.createElement('ul')
 const li = document.createElement('li')
 const p = document.createElement('p')
 const catalog = document.querySelector('#brew-card')
-
+const type = document.querySelector('#submit-type')
+const state = document.querySelector('#submit-state')
+const city = document.querySelector('#submit-city')
+const brewName = document.querySelector('#submit-name')
+const inputType = document.querySelector('#input-type')
 
 
 // Add version of this request to an event listener for the 'submit' button
@@ -34,7 +38,45 @@ fetch('https://api.openbrewerydb.org/breweries')
     // Add forward and back buttons at the bottom to advance through searches
 })
 
-document.querySelector('#submit-type').addEventListener('click', e => {
+type.addEventListener('click', e => {
     e.preventDefault()
-    console.log('I was clicked')
+    console.log(inputType.value)
+    catalog.innerHTML = ''
+    fetch(`https://api.openbrewerydb.org/breweries?by_type=${inputType.value}&per_page=25`)
+    .then(response => response.json())
+    .then(item => {
+        for(item of item) {
+            const card = document.createElement('div')
+            const h3 = document.createElement('h3')
+            const ul = document.createElement('ul')
+            const li = document.createElement('li')
+            const p = document.createElement('p')
+            const catalog = document.querySelector('#brew-card')
+            card.classList.add('card')
+            catalog.appendChild(card)
+            card.appendChild(h3)
+            h3.innerText = item.name
+            card.appendChild(ul)
+            ul.appendChild(li)
+            li.innerText = `Address: ${item.street} \n City: ${item.city} \n State: ${item.state} \n Zip: ${item.postal_code} \n Phone: ${item.phone}`
+            card.appendChild(p)
+            p.innerText = `URL: ${item.website_url}`
+    }
+});
+inputType.value = ''
 })
+
+state.addEventListener('click', e => {
+    e.preventDefault()
+    console.log('state was clicked')
+});
+
+city.addEventListener('click', e => {
+    e.preventDefault()
+    console.log('city was clicked')
+});
+
+brewName.addEventListener('click', e => {
+    e.preventDefault()
+    console.log('brewName was clicked')
+});
