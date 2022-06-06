41673c2a8d09e6fcd03d3747acfa6395751f3b23
Addison <addison.frei@icloud.com>
2022-06-06 13:58:27 -0400

    fetch added. Brewery list created in cards

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-// Create form boxes with options to search by: name, city, state, type.  
+// Consts
+const card = document.createElement('div')
+const h3 = document.createElement('h3')
+const ul = document.createElement('ul')
+const li = document.createElement('li')
+const p = document.createElement('p')
+const catalog = document.querySelector('#brew-card')
+
 
 
 // Add version of this request to an event listener for the 'submit' button
 fetch('https://api.openbrewerydb.org/breweries')
 .then(response => response.json())
-.then(data => {
-    console.log(data)
-    console.log('Name:', data[14].name)
-    console.log('Street:', data[14].street)
-    console.log('City:', data[14].city)
-    console.log('State:', data[14].state)
-    console.log('Zip:', data[14].postal_code)
-    console.log('Phone:', data[14].phone)
-    console.log('URL:', data[14].website_url)
+.then(item => {
+    for(item of item) {
+        const card = document.createElement('div')
+        const h3 = document.createElement('h3')
+        const ul = document.createElement('ul')
+        const li = document.createElement('li')
+        const p = document.createElement('p')
+        const catalog = document.querySelector('#brew-card')
+        card.classList.add('card')
+        catalog.appendChild(card)
+        card.appendChild(h3)
+        h3.innerText = item.name
+        card.appendChild(ul)
+        ul.appendChild(li)
+        li.innerText = `Address: ${item.street} \n City: ${item.city} \n State: ${item.state} \n Zip: ${item.postal_code} \n Phone: ${item.phone}`
+        card.appendChild(p)
+        p.innerText = `URL: ${item.website_url}`
+    }
+   
     // Create card for each object and add the info listed above to elements.  Add h2 for name of brewery, ul and li for the address, phone, url.
     // Add forward and back buttons at the bottom to advance through searches
+})
+
+document.querySelector('#submit-type').addEventListener('click', e => {
+    e.preventDefault()
+    console.log('I was clicked')
 })
