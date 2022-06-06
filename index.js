2c5fe8d07f2bd73dc88731e46567efd72c6202c8
Addison <addison.frei@icloud.com>
2022-06-06 14:38:35 -0400

    Search by city event added

--- index.js
+++ index.js
@@ -98,8 +98,31 @@ inputState.value = ''
 
 city.addEventListener('click', e => {
     e.preventDefault()
-    console.log('city was clicked')
+    console.log(inputCity.value)
+    catalog.innerHTML = ''
+    fetch(`https://api.openbrewerydb.org/breweries?by_city=${inputCity.value}&per_page=25`)
+    .then(response => response.json())
+    .then(item => {
+        for(item of item) {
+            const card = document.createElement('div')
+            const h3 = document.createElement('h3')
+            const ul = document.createElement('ul')
+            const li = document.createElement('li')
+            const p = document.createElement('p')
+            const catalog = document.querySelector('#brew-card')
+            card.classList.add('card')
+            catalog.appendChild(card)
+            card.appendChild(h3)
+            h3.innerText = item.name
+            card.appendChild(ul)
+            ul.appendChild(li)
+            li.innerText = `Address: ${item.street} \n City: ${item.city} \n State: ${item.state} \n Zip: ${item.postal_code} \n Phone: ${item.phone}`
+            card.appendChild(p)
+            p.innerText = `URL: ${item.website_url}`
+    }
 });
+inputCity.value = ''
+})
 
 brewName.addEventListener('click', e => {
     e.preventDefault()
