9fa67d0b983b89535034352acb260d7345c0ff0e
Addison <addison.frei@icloud.com>
2022-06-06 12:58:45 -0400

    search fields added

---
+++ README.md
@@ -0,0 +1,14 @@
+# Your Project Title
+This is an overview of what your project is about.  You can say this was a project from Flatiron School
+
+### Requirements
+* json-server to run db.json file
+
+
+### Installation/ Usage
+
+* Fork/ Clone project
+* Run json-server with `json-server --watch db.json`
+* Open index.html in browser
+
+#### Add more info regarding app/project   Above are just examples of what can be used. 
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// Create form boxes with options to search by: name, city, state, type.  
+
+
+// Add version of this request to an event listener for the 'submit' button
 fetch('https://api.openbrewerydb.org/breweries')
 .then(response => response.json())
 .then(data => {
@@ -9,5 +13,6 @@ fetch('https://api.openbrewerydb.org/breweries')
     console.log('Zip:', data[14].postal_code)
     console.log('Phone:', data[14].phone)
     console.log('URL:', data[14].website_url)
-    
+    // Create card for each object and add the info listed above to elements.  Add h2 for name of brewery, ul and li for the address, phone, url.
+    // Add forward and back buttons at the bottom to advance through searches
 })
